07cf5d0f562f41a05a2e065b9465a635dd8537ff
nampeeramanu99@example.com <nampeeramanu99@example.com>
2024-12-09 10:24:37 +0300

    Initial commit

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express')
 //will help to push the environemntal data. config means making sure everything is in place
-require('dotenv/config')
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 
 //get method
-app.get("/", (req, res) => {
+app.get('/', (req, res)=>{
     res.send('We are on home');
-});
+})
+
+mongoose.connect(process.env.MONGODB_LINK, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(()=>{console.log('connected to the database')})
+.catch(err=>{console.log('ERROR',err)});
+console.log("mongodb+srv://Manuella:emma247@anneroy.olfao.mongodb.net/", process.env.MONGODB_LINK);
 
 app.listen(process.env.PORT, ()=>{console.log('connected to port')})
 
